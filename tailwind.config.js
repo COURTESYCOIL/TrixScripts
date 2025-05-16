6e76285628d08f1a901bf137eaf860b4ff32d660
COURTESYCOIL <forbusiness6886543@gmail.com>
2025-05-16 19:37:36 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'title-gradient': 'linear-gradient(to right, #3b82f6, #06b6d4)',
+      },
+    },
+  },
+  plugins: [],
+};
